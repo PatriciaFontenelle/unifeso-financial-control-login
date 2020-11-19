c2f7e6d53ae4f0183b119d5f40e48fe2c73c0a50
Patrícia Fontenelle <patricia.fontenelle@hotmail.com>
2020-11-19 17:47:13 -0300

    comit de teste

---
+++ src/pages/cadastro/CadastroUsuario.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class CadastroUsuario extends React.Component{
+    render(){
+        return(
+            <div>
+                Cadastro de Usuário
+            </div>
+        )
+    }
+}
+
+export default CadastroUsuario;
